ef3da91fdc5b8f0590dd624e11bbd5c2318c951c
Ruslan <saxanda@gmail;.com>
2023-01-12 00:30:43 +0100

    Higher Order Function

--- src/main/scala/app/Calc.scala
+++ src/main/scala/app/Calc.scala
@@ -5,7 +5,7 @@ object Calc {
   def add(a: Int, b: Int) = a + b
 
   def div(a: Int, b: Int) = a / b
-
+/////////////////////////////////////////////
   def maybeItWillReturnSomething(flag: Boolean): Option[String] = {
     if (flag) Some("Found value") else None
   }
@@ -14,7 +14,8 @@ object Calc {
   val aClass = new ClassWithValParameter("Gandalf")
 
 def main(args: Array[String]) = {
-  val valAdd = add(1,2)
+  ///////////////////////////////
+ /* val valAdd = add(1,2)
   println(valAdd)
   val aClass = new ClassWithValParameter("Gandalf")
   println(aClass.name)
@@ -43,5 +44,43 @@ def main(args: Array[String]) = {
     case None => 0.0
   }
   println(value3)
+  object Greeting {
+    def english = "Hi"
+
+    def espanol = "Hola"
+  }
+  val x = Greeting
+  val y = x
+   println( x eq y)*/
+
+  //////////////////////////////////////////////
+ /* def lambda = { x: Int => x + 1 }
+  def lambda2 = (x: Int) => x + 2
+  val lambda3 = (x: Int) => x + 3
+
+  val lambda4 = new Function1[Int, Int] {
+    def apply(v1: Int): Int = v1 - 1
+  }
+
+  def lambda5(x: Int) = x + 1
+
+  val result = lambda(3)
+  val result1andhalf = lambda.apply(3)
+
+  val result2 = lambda2(3)
+  val result3 = lambda3(3)
+  val result4 = lambda4(3)
+  val result5 = lambda5(3)*/
+
+  ////////////////////////////////////////
+
+  def addWithoutSyntaxSugar(x: Int): Function1[Int, Int] = {
+    new Function1[Int, Int]() {
+      def apply(y: Int): Int = x + y
+    }
+  }
+
+  addWithoutSyntaxSugar(1).isInstanceOf[Function1[Int, Int]]
+  addWithoutSyntaxSugar(2)(3)
 }
 }
